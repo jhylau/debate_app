234525dedab2b8f4305aa9186376878f7edc9f9f
jhylau <jhylau1@gmail.com>
2013-06-25 16:04:11 -0700

    refactored helper for debate

--- app/helpers/_debate_helper.rb
+++ app/helpers/_debate_helper.rb
@@ -1,6 +1,6 @@
 module DebateHelper
   def username(debate_side)
-    if debate_side.first == nil
+    if debate_side.first.user == nil
       return 'TBD'
     else
       return debate_side.first.user.username
